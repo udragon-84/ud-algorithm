df9ffabd528ce86dd2da0920d10709155918bce9
ChangKeunYoo <ckyou@konkuk.ac.kr>
2024-08-12 15:44:24 +0900

    allganize test

---
+++ src/main/java/algorithm/test/Allganize_Solution1.java
@@ -0,0 +1,35 @@
+package algorithm.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Allganize_Solution1 {
+
+    public int solution(int[] arr) {
+        // code goes here
+
+        int rotationStandard = arr[0];
+        List<Integer> numList = new ArrayList<>();
+
+        for (int i = rotationStandard; i < arr.length; i++) {
+            numList.add(arr[i]);
+        }
+
+        for (int i = 0; i < rotationStandard; i++) {
+            numList.add(arr[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numList.size(); i++) {
+            sb.append(numList.get(i));
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        Allganize_Solution1 solution1 = new Allganize_Solution1();
+        int result = solution1.solution(new int[]{2, 3, 4, 1, 6, 10});
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/algorithm/test/Allganize_Solution2.java
@@ -0,0 +1,39 @@
+package algorithm.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Allganize_Solution2 {
+
+    public String solution(String str) {
+        String[] words = str.split(" ");
+        String result = "-1";
+        int maxRepeats = 0;
+
+        for (String word : words) {
+            int repeats = getWordMaxCount(word);
+            if (repeats > maxRepeats) {
+                maxRepeats = repeats;
+                result = word;
+            }
+        }
+        return maxRepeats > 1 ? result : "-1";
+    }
+
+    private int getWordMaxCount(String word) {
+        Map<Character, Integer> letterCount = new HashMap<>();
+        int maxCount = 0;
+        for (char c : word.toCharArray()) {
+            letterCount.put(c, letterCount.getOrDefault(c, 0) + 1);
+            maxCount = Math.max(maxCount, letterCount.get(c));
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Allganize_Solution2");
+        Allganize_Solution2 main = new Allganize_Solution2();
+        String result = main.solution("Hello appleaaa pie");
+        System.out.println(result);
+    }
+}
